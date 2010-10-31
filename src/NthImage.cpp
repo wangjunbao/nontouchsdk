81ab8519ea1af45dbb7ff66c6784961f14f9930c
neil.li90 <neil.li90@6b3f5b0f-ac10-96f7-b72e-cc4b20d3e213>
2010-10-31 12:03:49 +0000

    更新基础类NthProcessObj NthImage NthVideo

--- src/NthImage.cpp
+++ src/NthImage.cpp
@@ -1,10 +1,92 @@
 /*
- * NthImage.cpp
+ * NthImageIO.cpp
  * CopyRight @South China Institute of Software Engineering,.GZU
  * Author: 
  * 2010/10/20
  */
 
+#ifndef DLL_FILE
+#define DLL_FILE
+#endif
+
+#include "NthImage.h"
+#include <memory.h>
+
+
+NthImage::NthImage()
+{
+	_srcImage = NULL ; 
+	memset (_strPath, 0, sizeof (_strPath)) ; 
+//	CImage img ; 
+}
+
+NthImage::NthImage (char * strPath) 
+{
+	memcpy (_strPath, strPath, strlen (strPath)) ; 
+}
+
+NthImage::NthImage (IplImage * image)
+{
+	_srcImage = cvCloneImage (image) ; 
+}
+
+NthImage::~NthImage()
+{
+}
+
+int NthImage::Create(int height, int width) 
+{
+	CvSize size;
+	
+	size.width  = width;
+	size.height = height;
+
+	//创建图像数据
+	_srcImage = cvCreateImage( size, IPL_DEPTH_8U, 1 ) ;
+
+	return 0 ; 
+}
+
+int NthImage::Load () 
+{
+	_srcImage = cvLoadImage (_strPath, 1) ; 
+	return 0 ; 
+}
+
+int NthImage::Load (char * strPath)
+{
+	_srcImage = cvLoadImage (_strPath, 1) ; 
+	return 0 ; 
+}
+
+
+int NthImage::Save ()
+{
+	cvSaveImage (_strPath, _srcImage) ; 
+	return 0 ; 
+}
+
+int NthImage::SaveAs (char * strPath) 
+{
+	cvSaveImage (strPath, _srcImage) ;
+	return 0 ; 
+}
+
+int NthImage::SavaAs (void * pImage, char * strPath) 
+{
+	cvSaveImage (strPath, pImage) ; 
+	return 0 ; 
+}
+
+
+
+/*
+ * NthImage.cpp
+ * CopyRight @South China Institute of Software Engineering,.GZU
+ * Author: 
+ * 2010/10/20
+ */
+/*
 #ifndef DLL_FILE
 #define DLL_FILE
 #endif
@@ -55,3 +137,4 @@ IplImage *NthImage::image()
 	return _srcImage;
 }
 
+*/
--- src/NthProcessObject.cpp
+++ src/NthProcessObject.cpp
@@ -5,16 +5,36 @@
  * 2010/10/20
  */
 
+
 #ifndef DLL_FILE
 #define DLL_FILE
 #endif
 
 #include "NthProcessObject.h"
 
-NthProcessObject::NthProcessObject(void)
+NthProcessObject::NthProcessObject()
 {
+	_srcImage = NULL ; 
 }
 
-NthProcessObject::~NthProcessObject(void)
+NthProcessObject::~NthProcessObject()
 {
+	if (NULL != _srcImage)
+		cvReleaseImage(&_srcImage ) ; 
+}
+
+IplImage * NthProcessObject::image () 
+{
+	return _srcImage ; 
+}
+
+int NthProcessObject::height ()
+{
+	return _height = _srcImage ->roi ? _srcImage->height : _srcImage->roi->height ; 
+}
+
+int NthProcessObject::width ()
+{
+	return _width = _srcImage ->roi ? _srcImage->width : _srcImage->roi->width;
+; 
 }
