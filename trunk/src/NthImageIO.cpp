67f300012b8a1ae9ebbc264451f10fd71675a734
neil.li90 <neil.li90@6b3f5b0f-ac10-96f7-b72e-cc4b20d3e213>
2010-10-31 12:03:49 +0000

    更新基础类NthProcessObj NthImage NthVideo

--- trunk/src/NthImageIO.cpp
+++ trunk/src/NthImageIO.cpp
@@ -9,17 +9,51 @@
 #define DLL_FILE
 #endif
 
-#include <iostream>
-#include <highgui.h>
 #include "NthImageIO.h"
-#include "NthImage.h"
+#include <memory.h>
 
-using namespace std;
 
-NthImageIO::NthImageIO(void)
+NthImageIO::NthImageIO()
+{
+	CImage img ; 
+}
+
+NthImageIO::NthImageIO (char * strPath) 
+{
+	memcpy (_strPath, strPath, strlen (strPath)) ; 
+}
+
+NthImageIO::~NthImageIO()
 {
 }
 
-NthImageIO::~NthImageIO(void)
+int NthImageIO::LoadImage () 
 {
+	_srcImage = cvLoadImage (_strPath, 1) ; 
+	return 0 ; 
+}
+
+int NthImageIO::LoadImage (char * strPath)
+{
+	_srcImage = cvLoadImage (_strPath, 1) ; 
+	return 0 ; 
+}
+
+
+int NthImageIO::SaveImage ()
+{
+	cvSaveImage (_strPath, _srcImage) ; 
+	return 0 ; 
+}
+
+int NthImageIO::SaveImageAs (char * strPath) 
+{
+	cvSaveImage (strPath, _srcImage) ;
+	return 0 ; 
+}
+
+int NthImageIO::SavaImageAs (void * pImage, char * strPath) 
+{
+	cvSaveImage (strPath, pImage) ; 
+	return 0 ; 
 }
